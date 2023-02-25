d950041501a510cf2275cae21e2a7d71cd374ea1
sujal_shrestha <sujal.scxi121@amv.edu.np>
2023-02-25 10:06:00 +0545

    Completed Challenge 2

--- starter/class/character.js
+++ starter/class/character.js
@@ -2,15 +2,24 @@ class Character {
 
   constructor(name, description, currentRoom) {
     // Fill this in
+    this.name=name;
+    this.description=description;
+    this.currentRoom=currentRoom;
+    this.items=[];
+    this.health=100;
+    this.strength=10;
 
   }
 
   applyDamage(amount) {
     // Fill this in
+    this.health=this.health-amount;
   }
 
   die() {
     // Fill this in
+    this.items.splice(0,this.items.length);
+    this.currentRoom=null;
   }
 
 }
